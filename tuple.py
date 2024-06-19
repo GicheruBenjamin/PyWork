93d37de450c4faeca01f4e41f5850b741f3dc9de
GicheruBenjamin <projectbuild201@gmail.com>
2024-06-19 12:48:40 +0300

    C12.48

---
+++ tuple.py
@@ -0,0 +1,10 @@
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple) 
+
+print(my_tuple.count(3)) 
+
+print(my_tuple.index(3))  
+
+# Tuples are immutable, so you can't add, remove, or modify elements
+#my_tuple.add(6)  # This will raise an AttributeError
