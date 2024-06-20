8668caf96228ab007ee2347fb84df03a2d548fab
GicheruBenjamin <projectbuild201@gmail.com>
2024-06-20 13:40:53 +0300

    C1.40

--- enum.py
+++ enum.py
@@ -1,6 +1,20 @@
-from typing import List 
+'''from typing import List 
 
 my_snacks:List[str] = ["soda", "cookies", "biscuits", "cereal"]
 
 for i , snack in enumerate(my_snacks):
-    print(i, snack)
+    print(i, snack)'''
+    
+from enum import Enum
+
+class teachers(Enum):
+    HOD = "head of department"
+    LEC= "lecturer"
+    ASSIST = "assistant"
+    PROF = "professor"
+    ASSPROf = "assistant professor"
+    INST = "instructor"
+    TR = "teacher"
+    OTH = "other"
+    
+print(teachers.LEC.value) # Output "lecturer"
