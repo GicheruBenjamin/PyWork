6c5967e85cf35e577dfb8687fef5a92b5215a65a
GicheruBenjamin <projectbuild201@gmail.com>
2024-06-20 13:45:29 +0300

    C1.45

--- enum.py
+++ enum.py
@@ -1,9 +1,9 @@
-'''from typing import List 
+from typing import List 
 
 my_snacks:List[str] = ["soda", "cookies", "biscuits", "cereal"]
 
 for i , snack in enumerate(my_snacks):
-    print(i, snack)'''
+    print(i, snack)
     
 from enum import Enum
 
