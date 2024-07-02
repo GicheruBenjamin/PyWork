7be2fcbf69d4c15130ffe6f1464bc5d037838c62
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-02 11:42:20 +0300

    11.42

--- filer.py
+++ filer.py
@@ -2,3 +2,8 @@
 # Reading a file the usual way..
 file = open('note.txt' , 'r')
 c = file.read(); print(c) ; file.close()
+
+# Using with statement
+with open('note.txt' , 'r') as file:
+    c = file.read()
+    print(c)
