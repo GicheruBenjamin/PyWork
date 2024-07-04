c096034d960d62240db79bd805ca004045d6f7f7
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-04 16:04:22 +0300

    C4.04

--- cl.py
+++ cl.py
@@ -33,3 +33,42 @@ my_working_department.add_employee("John")
 my_working_department.add_employee("Jane")
 my_working_department.no_of_employees()
 my_working_department.list_employees()
+
+
+
+
+class Address:
+    def __init__(self, street, city, zipcode):
+        self.street = street
+        self.city = city
+        self.zipcode = zipcode
+
+    def get_full_address(self):
+        return f"{self.street}, {self.city}, {self.zipcode}"
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Employee:
+    def __init__(self, name, age, address):
+        self.person = Person(name, age)
+        self.address = Address(address['street'], address['city'], address['zipcode'])
+
+    def get_employee_info(self):
+        return f"Name: {self.person.name}, Age: {self.person.age}, Address: {self.address.get_full_address()}"
+
+# Creating instances and accessing attributes
+employee_info = {
+    'name': 'John Smith',
+    'age': 30,
+    'address': {
+        'street': '123 Main St',
+        'city': 'Anytown',
+        'zipcode': '12345'
+    }
+}
+
+employee = Employee(**employee_info)
+print(employee.get_employee_info())
