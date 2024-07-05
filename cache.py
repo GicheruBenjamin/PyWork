46f6ae87d4679851aa237cdf1013bc2fd04f87f3
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-05 15:00:58 +0300

    C3.00

---
+++ cache.py
@@ -0,0 +1,11 @@
+
+
+from functools import lru_cache
+
+'''def fib(n: int) -> int:
+    if n == 0 or n == 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+for i in range(40):
+    print(f"fib({i}) = {fib(i)}")'''
