390d7f8a967fe2d3e767149fb85b972080ad3bea
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-05 15:03:46 +0300

    C3.03

--- cache.py
+++ cache.py
@@ -9,3 +9,16 @@ from functools import lru_cache
 
 for i in range(40):
     print(f"fib({i}) = {fib(i)}")'''
+    
+
+@lru_cache(maxsize=3) 
+def fib(n: int) -> int:
+    if n == 0 or n == 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+for i in range(40):
+    print(f"fib({i}) = {fib(i)}")       
+    
+for i in range(40):
+    print(f"fib({i}) = {fib(i)}")
