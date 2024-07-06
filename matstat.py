92ea972628cc0b6e9cff874ffcd9b0826dd4f418
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-06 21:53:13 +0300

    c.53

--- matstat.py
+++ matstat.py
@@ -1,1 +1,9 @@
-
+from typing import List
+from statistics import mean , mode , median
+stud_marks: List[int] = [50, 62, 71, 84, 29, 45]
+mean_marks: int = mean(stud_marks)
+median_marks: int = median(stud_marks)
+mode_marks: int = mode(stud_marks)
+print(mean_marks)
+print(median_marks)
+print(mode_marks)
