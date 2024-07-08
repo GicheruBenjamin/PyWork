82ec547d3a4133b689f0ce864bcf3fedb826cd59
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-08 14:53:58 +0300

    C2.53

---
+++ percomb.py
@@ -0,0 +1,21 @@
+
+
+from itertools import combinations, permutations
+
+# Example set of elements
+elements = [1, 2, 3]
+
+# Generate and print combinations of size 2
+print("Combinations of size 2:")
+for combo in combinations(elements, 2):
+    print(combo)
+
+# Generate and print all permutations of the elements
+print("\nAll permutations:")
+for perm in permutations(elements):
+    print(perm)
+
+# Generate and print permutations of size 2
+print("\nPermutations of size 2:")
+for perm in permutations(elements, 2):
+    print(perm)
