df9587c582fe39e93fcfc21b90cc71bd439f7d94
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-15 18:19:22 +0300

    C6.19

--- dicts.py
+++ dicts.py
@@ -61,3 +61,9 @@ print(my_dict_copy)
 # Clearing a dictionary
 my_dict_copy.clear()
 print(my_dict_copy)  
+# Normal way of creating a dict
+no_of_fruits:dict = {"apples": 5, "oranges": 3, "bananas": 7}
+print(no_of_fruits)
+#Another way of doing so
+no_of_fruits = dict(apples=5, oranges=3, bananas=7)
+print(no_of_fruits)#Same output as b4
