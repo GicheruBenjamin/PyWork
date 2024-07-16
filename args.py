18da1cb5a40d2b095b9292f41ac14a5e539d6e2d
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-16 13:59:18 +0300

    C1.59

--- args.py
+++ args.py
@@ -1,1 +1,16 @@
 
+# Having the option for a lot of arguments in your functions
+
+#Normal function
+def work(time: tuple[str,float], weather: str) -> str:
+    return f"I worked on {time[0]} at {time[1]} and it was {weather}"
+
+my_day = work(("Monday", 8), "sunny")
+print(my_day)
+
+# Same function with args in the function creation
+def hiswork(*args):
+    return f"I worked on {args[0]} at {args[1]} and it was {args[2]}"
+
+dani_day = hiswork("Tuesday", 8, "windy")
+print(dani_day)
