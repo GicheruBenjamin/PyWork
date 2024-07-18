4f6804b77365a3e2218470d35f15b49829e0783c
GicheruBenjamin <projectbuild201@gmail.com>
2024-07-18 13:25:31 +0300

    C1.25

---
+++ math.py
@@ -0,0 +1,30 @@
+import math
+
+print(math.sqrt(16))   # Output: 4.0
+print(math.pow(2, 3))  # Output: 8.0
+print(math.exp(2))     # Output: 7.38905609893065
+print(math.log(10))    # Output: 2.302585092994046
+print(math.log10(100)) # Output: 2.0
+
+
+
+print(math.sin(math.pi/2))   # Output: 1.0
+print(math.cos(math.pi))      # Output: -1.0
+print(math.tan(math.pi/4))    # Output: 0.9999999999999998
+print(math.asin(1))          # Output: 1.5707963267948966
+print(math.acos(0))          # Output: 1.5707963267948966
+print(math.atan(1))          # Output: 0.7853981633974483
+print(math.degrees(math.pi))  # Output: 180.0
+print(math.radians(180))       # Output: 3.141592653589793
+		
+  
+print(math.ceil(3.14))  # Output: 4
+print(math.floor(3.14)) # Output: 3
+print(math.trunc(3.14)) # Output: 3
+print(math.factorial(5)) # Output: 120
+print(math.gcd(10, 15)) # Output: 5
+print(math.lcm(10, 15)) # Output: 120
+print(math.modf(3.14))  # Output: (0.14000000000000012, 3.0)
+print(math.remainder(10, 3)) # Output: 1.0
+print(math.fmod(10, 3)) # Output: 1.0
+print(math.pow(2, 3)) # Output: 8.0
